a7365541646b75a8ef3126f9dd73fcf8d447e1a6
fassetar <fassetar@gmail.com>
2025-06-24 10:48:55 -0400

    Add project files.

---
+++ SqlMaintence/CheckNullColumns.sql
@@ -0,0 +1,16 @@
+﻿DECLARE @TableName NVARCHAR(MAX) = 'YourTableName';
+DECLARE @SQL NVARCHAR(MAX) = '';
+DECLARE @Cols NVARCHAR(MAX) = '';
+
+SELECT @Cols = STRING_AGG(
+    'SUM(CASE WHEN [' + COLUMN_NAME + '] IS NULL THEN 1 ELSE 0 END) AS [Null_' + COLUMN_NAME + ']',
+    ', '
+)
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = @TableName;
+
+SET @SQL = '
+SELECT COUNT(*) AS TotalRows, ' + @Cols + '
+FROM ' + QUOTENAME(@TableName) + ';';
+
+EXEC sp_executesql @SQL;
